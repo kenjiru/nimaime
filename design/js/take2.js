79a81c497f63c1c522726b4f36832be0349129c0
kenjiru <kenjiru.ro@gmail.com>
2008-01-23 00:15:01 +0000

    design: more work on HTML and JavaScript.

--- design/js/take2.js
+++ design/js/take2.js
@@ -1,41 +1,80 @@
 var formShowed = null;
 	
-	function showForm (form) 
-	{
-		// daca este activ un form
-		if ($defined (formShowed)) {
-			formShowed.setStyle ('display', 'none');
-			if (formShowed == form) {
-				formShowed = null;
-				resizeLeftPanel ();
-				return;
-			}
+function showForm (form) 
+{
+	// daca deja este afisat un form, il ascundem
+	if ($defined (formShowed)) {
+		formShowed.setStyle ('display', 'none');
+		if (formShowed == form) {
 			formShowed = null;
+			resizeLeftPanel ();
+			return;
 		}
-		form.setStyle ('display', 'block');
-		formShowed = form;
-		
-		resizeLeftPanel ();
+		formShowed = null;
 	}
+	form.setStyle ('display', 'block');
+	formShowed = form;
 	
-	function resizeLeftPanel ()
-	{
-		var rightHeight = $('rightPanel').getCoordinates().height;
-		var rightHeight2 = $('rightPanel').getStyle('height');
-		
-		$('leftPanel').setStyle('height', rightHeight2);
+	resizeLeftPanel ();
+}
+
+function resizeLeftPanel ()
+{
+	var rightHeight = $('rightPanel').getCoordinates().height;
+	var rightHeight2 = $('rightPanel').getStyle('height');
+	
+	$('leftPanel').setStyle('height', rightHeight2);
+	
+}
+
+// mouseover deasupra unui folder
+function folderMouseOver (folder)
+{
+	if (folder.className == 'folder') {
+		folder.removeClass ('folder');
+		folder.addClass ('folder-hover');
 	}
+}
+
+// mouseout deasupra unui folder
+function folderMouseOut (folder)
+{
+	if (folder.className == 'folder-hover') {
+		folder.removeClass ('folder-hover');
+		folder.addClass ('folder');
+	}
+}
+
+function init ()
+{
+	// atasam ev. pt. "optionsMenu"
+	$('detailsButton').addEvent ('click', function () {
+		showForm ($('detailsForm'));
+	});
+	$('uploadButton').addEvent ('click', function () {
+		showForm ($('uploadForm'));
+	});
 	
-	function init ()
-	{
-		$('detailsButton').addEvent ('click', function () {
-			showForm ($('detailsForm'));
+	// atasam ev. pt. "folders"
+	var folders = $('rootList').getChildren();
+	folders.each(function (folder) {
+		folder.addEvent ('mouseover', function () {
+			folderMouseOver (this);
 		});
-		$('uploadButton').addEvent ('click', function () {
-			showForm ($('uploadForm'));
+		folder.addEvent ('mouseout', function () {
+			folderMouseOut (this);
 		});
 		
-		resizeLeftPanel ();
-	}
+		// face el. non-selectabil, IE specific
+		folder.addEvent ('selectstart', function () {
+			return false;
+		});
+		folder.addEvent ('mousedown', function () {
+			return false;
+		});
+	});
 	
-	window.addEvent('domready', init);
+	resizeLeftPanel ();
+}
+
+window.addEvent('domready', init);
