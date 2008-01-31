da95e03232d111e4da47f4d23a9243c6e6b50f3e
kenjiru <kenjiru.ro@gmail.com>
2008-01-31 22:49:12 +0000

    upload: fake file input test (quirksmode.org tip)

--- upload/js/upload.js
+++ upload/js/upload.js
@@ -14,20 +14,44 @@ var MultiUpload = new Class({
 		// add element methods
 		$(input_element);
 		
+		// save the active input
+		this.active_input = input_element;
+		
 		this.initializeElement (input_element);
 		
 		// Files list
 		var container = new Element(
-			'div', {'class': 'multiupload'}
+			'div', {
+				'class': 'multiupload'
+			}
 		);
 		this.list = new Element(
-			'div', {'class':'list'}
+			'div', {
+				'class':'list'
+			}
 		);
-		container.injectAfter( input_element );
-		container.adopt( input_element );
-		container.adopt( this.list );
+		// file inputs
+		var fileinputs = new Element(
+			'div', {
+				'class':'fileinputs'
+			}
+		);
+		// fake input file
+		var fake_input = new Element(
+			'div', {
+				'class':'fakefile',
+			}
+		);
+		fileinputs.injectAfter(input_element);
+		fileinputs.adopt(fake_input);
+		fileinputs.adopt(input_element);
+		
+		container.injectAfter(fileinputs);
+		container.adopt(fileinputs);
+		container.adopt(this.list);
 	},
 	initializeElement: function(element) {
+		element.addClass('realfile');
 		element.addEvent(
 			'change',
 			function(){
@@ -51,6 +75,20 @@ var MultiUpload = new Class({
 		var id = this.lastid;
 		
 		var span = new Element('span').setText( name );
+		var img_ext = new Element(
+			'img',
+			{
+				'src':'img/cross_small.gif',
+				'class':'ext',
+				'alt':'Delete',
+				'title':'Delete',
+				'events':{
+					'click':function(){
+						this.deleteElement (id);
+					}.bind(this)
+				}
+			}
+		);
 		var img_delete = new Element(
 			'img',
 			{
@@ -69,7 +107,7 @@ var MultiUpload = new Class({
 			'div', {
 				'class':'item'
 			}
-		).adopt(img_delete).adopt(span);
+		).adopt(img_ext).adopt(img_delete).adopt(span);
 		
 		this.list.adopt(row);
 		
@@ -90,7 +128,8 @@ var MultiUpload = new Class({
 		
 		element.style.position = 'absolute';
 		element.style.left = '-1000px';
-		new_input.injectAfter( element );
+		new_input.injectAfter (element);
+		this.active_input = new_input;
 	},
 	deleteElement: function(id) {
 		this.elements[id].file.remove();
