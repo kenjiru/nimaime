6bd3630b4849ac2e308c27897fa459c3b24b5510
kenjiru <kenjiru.ro@gmail.com>
2009-07-05 14:37:46 +0000

    Small changes.

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -1,14 +1,13 @@
 <?php
-// the upload folder
+// The upload path containing the child upload folders
 $root_dir = 'files/';
 
-// Child folders and the extensions of the files allowed in those folders.
+// The upload folders and the extensions of the files allowed in those folders.
 // Specify multiple extensions separated by space, egg: ".jpg .jpeg .png".
 // Extensions are case insensitive.
 $allowed_dirs = array("Images" => ".jpg .jpeg .png",
 	"Movies" => ".flv",
-	"Text" => ".txt",
-	"Test" => "" 
+	"Text" => ".txt" 
 );
 
 // size for thumbnail images
@@ -19,13 +18,14 @@ $thumb_height = 160;
  * DO NOT EDIT BELOW
  */
 
+// TODO: this does not work in safe mode, see mkdir manual
+
 // check for the existence of the upload folder
-// TODO: try to create the upload folder
 if (false === is_dir($root_dir))
 	if (false === mkdir($root_dir))
 		die("Can not create the root_dir $root_dir");
 
-// TODO: this does not work in safe mode, see mkdir manual
+// create the child upload folders
 foreach (array_keys($allowed_dirs) as $dir) {
 	$child_dir = $root_dir. $dir;
 	if (false === is_dir($child_dir))
--- templates/browser-v2.tpl.php
+++ templates/browser-v2.tpl.php
@@ -4,7 +4,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-	<title>Browser ng</title>
+	<title>Upload manager</title>
 	<link rel="stylesheet" type="text/css" href="css/browser-v2.css"/>
 	<link rel="stylesheet" type="text/css" href="css/slides.css"/>
 	<link rel="stylesheet" type="text/css" href="css/upload.css"/>
